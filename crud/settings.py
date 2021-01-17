11245a3ed9a813b9ebbe7eab00237968273548ee
DorcasToto <dorcasmosonik@gmail.com>
2021-01-17 14:00:47 +0300

    configure postgres database

--- crud/settings.py
+++ crud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'veterinary'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'veterinary',
+        'USER': 'dorcas',#change your username
+        'PASSWORD': '',# change your password,
     }
 }
 
