3c8158ccee917d8ff4e9120db95d82a15d59e170
DorcasToto <dorcasmosonik@gmail.com>
2021-01-17 21:38:07 +0300

    registration functionality

--- veterinary/urls.py
+++ veterinary/urls.py
@@ -2,12 +2,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.urls import path,include
 
 urlpatterns = [
     path('',views.index,name = 'home'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('register/',views.register, name='registration'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('accounts/register/',views.register, name='registration'),
   
 ]
 
--- veterinary/views.py
+++ veterinary/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from . forms import RegistrationForm
+from django.contrib import messages
+from .models import *
+from django.shortcuts import redirect
 
 # Create your views here.
-def index():
-    pass
+def index(request):
+    return render(request,'index.html')
 
-def register():
-    pass
+def register(request):
+
+    if request.method == 'POST':
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+
+    else:
+        registration_form= RegistrationForm()
+
+    return render(request, 'register.html', {'registration_form': registration_form})
